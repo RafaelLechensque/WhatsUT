f9b4cc0c846c1a09765e055980cfcf045137de47
Rafael Lechensque <rafaelaquino@alunos.utfpr.edu.br>
2025-06-20 15:57:56 -0300

    iniciando repo

---
+++ src/main.ts
@@ -0,0 +1,21 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('ZAP ZAP 2')
+    .setDescription('bora passar')
+    .setVersion('1.0')
+    // .addTag('cats')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
+  await app.listen(process.env.PORT ?? 3000);
+  console.log(`esta rodando em http://localhost:${3000}/api`);
+}
+bootstrap();
